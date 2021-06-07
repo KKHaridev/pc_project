54f2d33e902d9a78107bb574030c7f3469a609a8
K K Haridev <67409998+KKHaridev@users.noreply.github.com>
2021-06-07 17:58:40 +0530

    alaphuzha set

--- base/models.py
+++ base/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.db.models.fields import CharField
 # Create your models here.
+class NameField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        super(NameField, self).__init__(*args, **kwargs)
+
+    def get_prep_value(self, value):
+        return str(value).upper()
+
 class hospitals(models.Model):
     name = models.CharField(max_length=100)
-    district = models.CharField(max_length=20)
-    beds = models.IntegerField()
+    district = NameField(max_length=20)
+    beds_available = models.IntegerField()
     def __str__(self):
         return self.name
+
+
+
+
