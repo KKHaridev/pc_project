bad34730061f127e278106a0a03f9f8e8f8bf481
K K Haridev <67409998+KKHaridev@users.noreply.github.com>
2021-06-07 17:02:08 +0530

    first

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import hospitals
 
 # Create your views here.
-a = 'alappuzha'
+
 def home(request):
     hospital = hospitals.objects.all()
     return render(request , 'base/index.html',{'hospitals' : hospital})
@@ -11,5 +11,5 @@ def register(request):
     return render(request , 'base/register.html')
 
 def alappuzha(request):
-    hospital = hospitals.objects.filter(district=a)
+    hospital = hospitals.objects.all()
     return render(request , 'base/alappuzha.html',{'hospitals' : hospital})
